8433eab89b90253d1da3e27859ab6b92d432a82d
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-02-11 15:59:04 +0530

    feat(user-auth): implement bcrypt for password hashing

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,8 +1,10 @@
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 const postApiv1Registered = async (req, res) => {
   const { handle, email, password, category } = req.body;
   console.log(req.body);
+  const enEncPassword = await bcrypt.hash(password, 10);
   try {
     const defaulLink = {
       url: "https://github.com/saurabhjaykar1603",
@@ -12,13 +14,13 @@ const postApiv1Registered = async (req, res) => {
     const user = await User.create({
       handle,
       email,
-      password,
+      password: enEncPassword,
       roll: category,
       links: [defaulLink],
     });
 
     const token = jwt.sign({ email: email }, process.env.JWT_SECRETE_KEY);
-
+    user.password = undefined;
     return res.status(200).json({
       success: true,
       id: user._id,
@@ -42,9 +44,18 @@ const postApiv1Login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const user = User.findOne({ email, password });
+    const user = await User.findOne({ email });
     console.log(user);
     if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+    // Compare the provided password with the hashed password stored in the database
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
       return res.status(401).json({
         success: false,
         message: "Invalid credentials",
