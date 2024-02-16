5dc9be4d971b0de88f7dfa86129eafa9e4f3d7f9
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-02-16 21:58:38 +0530

    fixed : remove soicalmedia api from server

--- server/controllers/getUserData.js
+++ server/controllers/getUserData.js
@@ -23,28 +23,4 @@ const getApiV1UserData = async (req, res) => {
   }
 };
 
-
-const getApiV1SocialMedia = async (req, res) => {
-  const handle = req.params.handle;
-  try {
-    const user = await User.findOne({ handle });
-    if (!user) {
-      return res.status(404).json({
-        success: false,
-        message: "User not found",
-      });
-    }
-    res.status(200).json({
-      success: true,
-      data: user,
-      message: "user found with links",
-    });
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      message: err.message,
-    });
-  }
-};
-
-export  {getApiV1UserData, getApiV1SocialMedia};
+export  {getApiV1UserData};
