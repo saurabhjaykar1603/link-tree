543f2028f1ff1fafe7493ecb00b8da21d7283b62
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2024-02-26 14:48:38 +0530

    feat: implement POST /api/v1/save/links endpoint integrated to save links

--- site/pages/edit/link.js
+++ site/pages/edit/link.js
@@ -1,13 +1,11 @@
 import UserHeader from "@/components/UserHeader";
 import axios from "axios";
-import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
 function link() {
   const [links, setLinks] = useState([{ url: "", title: "" }]);
   const [title, setTitle] = useState("");
-  const [url, setUrl] = useState("");
   const handleChange = (index, field, value) => {
     const updatedLinks = [...links];
     const LinkToUpdate = { ...updatedLinks[index], [field]: value };
@@ -21,6 +19,51 @@ function link() {
   };
   const handleAddLink = () => {
     setLinks([...links, { url: "", title: "" }]);
+  };
+
+  const saveLinks = async (e) => {
+    e.preventDefault();
+
+    // Convert links and title state values to arrays
+    const linksArray = Object.values(links);
+    const titleArray = Object.values(title);
+
+    // Create an array of objects from links and title
+    const linksData = linksArray.map((link, index) => ({
+      link: link,
+      title: titleArray[index],
+    }));
+
+    // Now linksData is an array of objects containing link and title pairs
+    console.log(linksData);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/v1/save/links",
+        {
+          tokenMail: localStorage.getItem("LinkTreeToken"),
+          links: linksData,
+        },
+        {
+          headers: {
+            "Content-type": "application/json",
+          },
+        }
+      );
+
+      if (response?.data.success) {
+        toast.success(" Links Loaded ");
+        console.log(response?.data?.handle);
+        setLinks(response?.data?.data);
+        window.location.href = `http://localhost:3000/${response?.data?.handle}`;
+      } else {
+        toast.error("Failed to  links");
+      }
+    } catch (error) {
+      const errorMessage =
+        error.response?.data.message || "Error loadind Links";
+      toast.error(errorMessage);
+    }
   };
 
   useEffect(() => {
@@ -42,7 +85,7 @@ function link() {
         if (response?.data.success) {
           toast.success(" Links Loaded ");
           console.log(response?.data?.data);
-          setLinks(response?.data?.data);
+          setLinks(response?.data?.data || []);
         } else {
           toast.error("Failed to  links");
         }
@@ -65,59 +108,72 @@ function link() {
               Edit Your Links
             </h1>
             <div>
-              <form action="" className="px-5">
-                {links.map((linkObj, index) => {
-                  const { title, url } = linkObj;
-                  return (
-                    <div
-                      key={index}
-                      className="flex  flex-wrap justify-evenly my-10 gap-y-5 border-2 border-blue-300 p-5 rounded-md sm:border-red-300 "
-                    >
-                      <label>
-                        <input
-                          type="text"
-                          value={url}
-                          onChange={(e) =>
-                            handleChange(index, "url", e.target.value)
-                          }
-                          className="outline-none border-2 border-blue-300 py-2  px-3 ml-3 rounded-md"
-                          placeholder="Enter your url"
-                        />
-                      </label>
-                      <label>
-                        <input
-                          type="text"
-                          value={title}
-                          onChange={(e) =>
-                            handleChange(index, "title", e.target.value)
-                          }
-                          className="outline-none border-2 border-blue-300 py-2  px-3 ml-3 rounded-md"
-                          placeholder="Enter your Title"
-                        />
-                      </label>
-                      <button
-                        type="button"
-                        onClick={() => {
-                          handleRemoveLink(index);
-                        }}
-                        className="bg-red-500 px-3 p-2 text-white font-bold rounded-md shadow-md sm:font-bold"
-                      >
-                        Remove Link
-                      </button>
-                    </div>
-                  );
-                })}
-                <div className=" flex justify-center flex-wrap gap-5">
+              <form action="" className="px-5 my-5">
+                <div className="overflow-x-auto">
+                  <table className="min-w-full bg-white border border-gray-300">
+                    <thead>
+                      <tr>
+                        <th className="py-2 px-4 border-b">Url</th>
+                        <th className="py-2 px-4 border-b">Title</th>
+                        <th className="py-2 px-4 border-b">Remove Button</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {links?.map((linkObj, index) => {
+                        const { title, url } = linkObj;
+                        return (
+                          <tr key={index}>
+                            <td className="py-2 px-4 border-b  ">
+                              <input
+                                type="text"
+                                value={url}
+                                onChange={(e) =>
+                                  handleChange(index, "url", e.target.value)
+                                }
+                                className="outline-none border-2  mx-auto border-blue-300 py-2 px-3  rounded-md w-full md:w-full"
+                                placeholder="Enter your url"
+                              />
+                            </td>
+                            <td className="py-2 px-4 border-b ">
+                              <input
+                                type="text"
+                                value={title}
+                                onChange={(e) =>
+                                  handleChange(index, "title", e.target.value)
+                                }
+                                className="outline-none border-2  mx-auto border-blue-300 py-2 px-3  rounded-md w-full md:w-full"
+                                placeholder="Enter your Title"
+                              />
+                            </td>
+                            <td className="py-2 px-4 border-b flex justify-center ">
+                              <button
+                                type="button"
+                                onClick={() => {
+                                  handleRemoveLink(index);
+                                }}
+                                className="outline-none border-2  mx-auto border-red-400 py-2  bg-red-300 px-3  rounded-md w-full md:w-full"
+                              >
+                                Remove Link
+                              </button>
+                            </td>
+                          </tr>
+                        );
+                      })}
+                    </tbody>
+                  </table>
+                </div>
+                <div className="flex justify-center flex-wrap gap-5 mt-5">
                   <button
                     type="button"
                     onClick={handleAddLink}
-                    className="w-1/2 bg-blue-500 px-2 p-2 text-white font-bold rounded-md shadow-md"
+                    className="w-full md:w-1/2 bg-blue-500 px-2 p-2 text-white font-bold rounded-md shadow-md"
                   >
                     Add Link
                   </button>
                   <button
                     type="button"
-                    className="w-1/2 bg-green-500 px-2 p-2 text-black font-bold rounded-md shadow-md"
+                    className="w-full md:w-1/2 bg-green-500 px-2 p-2 text-black font-bold rounded-md shadow-md"
+                    onClick={saveLinks}
                   >
                     Save Link
                   </button>
